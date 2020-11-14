df9aafab9324439784743d8ec48a1c8bcb621489
Daniel <d.tuytuyshkin@gmail.com>
2020-11-14 19:37:07 +0200

    added first component and clean up

--- src/App.js
+++ src/App.js
@@ -1,20 +1,8 @@
 import './App.css';
-import {MapContainer, TileLayer, Marker, Popup} from 'react-leaflet';
+import SimpleMap from './components/SimpleMap';
 
-function App() {
+export default function App() {
   return (
-    <MapContainer className="map" center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-      <TileLayer
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={[51.505, -0.09]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
-    </MapContainer>
+    <SimpleMap />
   );
 }
-
-export default App;
