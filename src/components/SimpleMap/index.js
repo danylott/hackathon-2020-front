658c249a8d7bd7144cddbb62703f5b7b7afcd0c8
kseniia-grishchenko <kseniarctybz@gmail.com>
2020-11-15 22:01:26 +0200

    connected to back-end, printed points on map

--- src/components/SimpleMap/index.js
+++ src/components/SimpleMap/index.js
@@ -1,18 +1,30 @@
 import './styles.css';
+import axios from 'axios';
+import React, {useEffect, useState} from 'react';
 import {MapContainer, TileLayer, Marker, Popup} from 'react-leaflet';
 
 export default function SimpleMap() {
+    const [points, setPoints] = useState([]);
+    useEffect(() => {
+        axios.get('http://127.0.0.1:8000/api/simple/point/')
+            .then(res => setPoints(res.data))
+            .catch(err => console.log(err));
+    }, [])
   return (
     <MapContainer className="simple-map" center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
       <TileLayer
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[51.505, -0.09]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
+        {points.map(point => (
+            <Marker position={[point.x, point.y]} key={point.id}>
+                {point.use_popup && (
+                    <Popup>
+                        {point.popup_message}
+                    </Popup>
+                )}
+            </Marker>
+        ))}
     </MapContainer>
   );
 }
